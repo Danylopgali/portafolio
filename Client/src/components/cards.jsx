d11eae6a7cd3695d23d0b716dd6dbcd357fae3d4
Danylopgali <daniellopg@hotmail.com>
2024-01-06 13:48:20 -0600

    separacion de cliente y api

---
+++ Client/src/components/cards.jsx
@@ -0,0 +1,34 @@
+// Cards.js
+import "./styles/cards.css"
+import React, { useState, useEffect } from 'react';
+import Card from './card';
+import axios from 'axios';
+
+function Cards() {
+  const [proyectos, setProyectos] = useState([]);
+
+  useEffect(() => {
+    const obtenerProyectos = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/all-proyects');
+        setProyectos(response.data);
+      } catch (error) {
+        console.error('Error al obtener proyectos desde el servidor:', error);
+      }
+    };
+
+    obtenerProyectos();
+  }, []); // La dependencia vacía asegura que se ejecute solo una vez al montar el componente
+
+  return (
+    <div className="Cards">
+      <div className="Cards_content">
+        {proyectos.map((proyecto) => (
+          <Card key={proyecto._id} proyecto={proyecto} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
---
+++ Client/src/components/proyects.jsx
@@ -0,0 +1,18 @@
+import Cards from "./cards";
+import "./styles/proyectos.css"
+function Proyects() {
+    return (
+      
+      <div id="proyectos"className="proyectos">
+      <div className="tituloP">
+      Proyectos
+      </div>
+      <div className="container">
+       <Cards/>
+      </div>
+      </div>
+    );
+  }
+  
+  export default Proyects;
+  
